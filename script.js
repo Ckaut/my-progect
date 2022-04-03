ed0344a83ed560905fbf8de6301fd4a403c7713c
Ckaut <77609761+Ckaut@users.noreply.github.com>
2022-04-03 16:49:30 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+//out.innerHTML = '<i><b>Hi</b></i>';
+//out.innerText = '<b>Hi</b>';
+//out.innerHTML = '<i><b>Hi</b></i>';
+//document.getElementById('out').innerHTML = 777;
+/*let p;
+p = document.getElementById('out');
+p = document.querySelector('#out'); //работают быстрее, чем предыдущая строка
+p.innerHTML = 555;
+console.log(p);
+let c = 'Hello';
+let d = 'world';
+console.log(c+d);*/
+
+
+// let year = 
+
+
+
+//window - глобальный объект 
+// alert(window.innerHeight); // внутренняя высота окна браузера
+
+// заменим цвет фона на красный,
+/*document.body.style.background = "red";
+
+// а через секунду вернём как было
+
+setTimeout(() => document.body.style.background = "", 1000);
+alert(location.href); // показывает текущий URL
+if (confirm("Перейти на Wikipedia?")) {
+  location.href = "https://wikipedia.org"; // перенаправляет браузер на другой URL
+} */
+let y=1;
+let x = y =2;
+alert(x)
